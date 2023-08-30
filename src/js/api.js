efcd9fe417e39353b0de5228c734d9db6c32a630
Sam Rawks <samrawks@live.com>
2023-08-31 00:43:17 +0100

    Added the button styles!

--- src/js/api.js
+++ src/js/api.js
@@ -23,7 +23,7 @@ async function buildSpecialist() {
   for (let i = 0; i < 10; i++) {
     document.getElementById('specialistContainer').innerHTML +=
     '<div class="specialist"><div class="col"><div class="specialist_info"><div class="specialist__photo"><img src="' + specialistArray[i].ProfileImage + '" alt=""></div><span class="specialist__name">'+ specialistArray[i].Title + '</span> — <span class="specialist__title">' + specialistArray[i].Specialty + '</span><div class="specialist__location">Based at: ' + specialistArray[i].Hospital + '&bullet; 9 Miles</div><div class="specialist__links"><a href="tel:' + specialistArray[i].PhoneNo + '">' + specialistArray[i].PhoneNo + '</a> <a href="">Go to hospital website</a> <a href="">Get directions</a></div></div><div class="specialist_desc">' + 
-    specialistArray[i].ProfessionalBackground + '<a href="">View less</a></div></div><div class="col"><button>View full profile</button><button>Book an appointment</button></div><hr></div>';
+    specialistArray[i].ProfessionalBackground + '<a href="">View less</a></div></div><div class="col"><button class="btn btn-secondary">View full profile</button><button class="btn btn-primary">Book an appointment</button></div><hr></div>';
   }
 
 }
