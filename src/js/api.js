cb0abe338a41fe9dbbaa8bc65cc77bdd2ac7014b
Sam Rawks <samrawks@live.com>
2023-08-31 13:43:12 +0100

    Styled the search results template.

--- src/js/api.js
+++ src/js/api.js
@@ -22,8 +22,8 @@ async function buildSpecialist() {
 
   for (let i = 0; i < 10; i++) {
     document.getElementById('specialistContainer').innerHTML +=
-    '<div class="specialist"><div class="col"><div class="specialist_info"><div class="specialist__photo"><img src="' + specialistArray[i].ProfileImage + '" alt=""></div><span class="specialist__name">'+ specialistArray[i].Title + '</span> — <span class="specialist__title">' + specialistArray[i].Specialty + '</span><div class="specialist__location">Based at: ' + specialistArray[i].Hospital + '&bullet; 9 Miles</div><div class="specialist__links"><a href="tel:' + specialistArray[i].PhoneNo + '">' + specialistArray[i].PhoneNo + '</a> <a href="">Go to hospital website</a> <a href="">Get directions</a></div></div><div class="specialist_desc">' + 
-    specialistArray[i].ProfessionalBackground + '<a href="">View less</a></div></div><div class="col"><button class="btn btn-secondary">View full profile</button><button class="btn btn-primary">Book an appointment</button></div><hr></div>';
+    '<div class="specialist"><div class="col"><div class="specialist_info--wrapper"><div class="specialist__photo"><img src="' + specialistArray[i].ProfileImage + '" alt=""></div><div class="specialist_info"><h3 class="specialist__name">'+ specialistArray[i].Title + ' — ' + specialistArray[i].Specialty + '</h3><div class="specialist__location"><i class="fa-solid fa-hospital"></i> Based at: ' + specialistArray[i].Hospital + ' &bullet; 9 Miles</div><div class="specialist__links"><a href="tel:' + specialistArray[i].PhoneNo + '"><i class="fa-solid fa-phone"></i> ' + specialistArray[i].PhoneNo + '</a> <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i> Go to hospital website</a> <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i> Get directions</a></div></div></div><div class="specialist_desc">' + 
+    specialistArray[i].ProfessionalBackground + '<a href="" class="specialist__accordion">View less</a></div></div><div class="col"><button class="btn btn-secondary">View full profile</button><button class="btn btn-primary">Book an appointment</button></div></div><hr>';
   }
 
 }
